7586bc5ef0e35269961773adb34736224bb99510
vanhuyne <thanvanhuyy@gmail.com>
2025-10-10 10:16:44 +0700

    feat: enhance webpack configuration to include fallbacks for browser compatibility

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,26 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   eslint: {
     ignoreDuringBuilds: true, // ✅ Bỏ qua lỗi ESLint khi build production
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve = config.resolve || {};
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+        child_process: false,
+        path: false,
+        crypto: false,
+        stream: require.resolve("stream-browserify"),
+        assert: require.resolve("assert"),
+        os: require.resolve("os-browserify/browser"),
+      };
+    }
+    return config;
   },
 };
 
